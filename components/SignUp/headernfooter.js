8959dcc35f9eb553c4992d5270eb6565786f0e83
chidalu okechukwu <73858189+Chidalu567@users.noreply.github.com>
2022-11-15 14:05:39 +1100

    built the Signup Screen for admins to signup

---
+++ components/SignUp/headernfooter.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import navimage from "../../images/StripeBranding.jpg";
+import Image from 'next/image';
+
+
+// Header UI
+const Navbar = styled.nav`
+    background-color:#262F36;
+    width:100%;
+    height:130px;
+`;
+
+export const Header = () => {
+    return (
+        <Navbar>
+            <Image src={navimage} priority="true" alt="brandlogo"  style={{width:"130px",height:"110px",margin:'10px',objectFit:'contain'}}/>
+        </Navbar>
+    )
+}
+
+
+
+
+
+
+// Footer UI
+const FootContainer = styled.footer`
+    width:100%;
+    height:130px;
+    background-color:#262F36;
+    @media screen and (min-width:720px){
+        position:absolute;
+        bottom:0px;
+    }
+`;
+
+export const Footer = () => {
+    return (
+        <FootContainer>
+        </FootContainer>
+    )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,113 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
+import styled from 'styled-components';
+import { Layout } from '../components/SignUp/layout.js';
+import { Checkout } from '../components/SignUp/components/checkout.js';
 
-export default function Home() {
+const Home = () => {
   return (
-   <h1>Login Page</h1>
+    <Container>
+      <Checkout status={{step1:'process'}} />
+      <Form>
+        <FormItem>
+          <Label htmlFor="firstname">Firstname:</Label>
+          <Input type="text" id="firstname" name="firstname" />
+        </FormItem>
+
+        <FormItem>
+          <Label htmlFor="lastname">Lastname:</Label>
+          <Input type="text" id="lastname" name="lastname" />
+        </FormItem>
+
+        <FormItem>
+          <Label htmlFor="email">Email:</Label>
+          <Input type="email" id="email" name="email"/>
+        </FormItem>
+
+        <FormItem>
+          <Label htmlFor="date">DateOfBirth:</Label>
+          <Input type="date" name="date" id="date"/>
+        </FormItem>
+
+        <FormItem>
+          <Label htmlFor="password">Password:</Label>
+          <Input type="text" id="password" name="password"/>
+        </FormItem>
+
+        <FormItem>
+          <Label htmlFor="file">Profile Photo:</Label>
+          <Upload type="file" name="file" id="file"/>
+        </FormItem>
+
+        <Submit type="button">Submit</Submit>
+      </Form>
+    </Container>
+  )
+}
+
+// UI Component
+const Container = styled.main`
+  margin:10px;
+`;
+
+const Form = styled.form`
+  display:flex;
+  flex-direction:column;
+  margin:auto auto;
+  @media screen and (min-width:720px){
+    width:1100px;
+  }
+`;
+
+const FormItem = styled.div`
+  display:flex;
+  flex-direction:row;
+  margin-bottom:40px;
+  margin:10px 10px;
+  justify-content:space-between;
+  @media screen and (max-width:720px){
+    flex-direction:column;
+    margin-bottom:30px;
+  }
+`;
+
+const Input = styled.input`
+    width:80%;
+    background-color:#262F36;
+    height:50px;
+    border-top-right-radius: 15px;
+    border: 1px solid transparent;
+    color:white;
+    padding-left:20px;
+    @media screen and (max-width:720px){
+      width:100%;
+    }
+`;
+
+const Label = styled.label`
+    font-family:Inter;
+    font-weight:bold;
+    font-size:20px;
+`;
+
+const Upload = styled.input``;
+
+const Submit = styled.button`
+  width:50%;
+  align-self:center;
+  padding:10px;
+  border:1px solid transparent;
+  background-color:#262F36;
+  color:white;
+  border-radius:10px;
+`;
+
+export default Home;
+
+Home.getLayout = function getLayout(content) {
+  return (
+    <Layout>
+      <div>{content}</div>
+    </Layout>
   )
 }
